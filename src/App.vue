ab7ddea047c5f06eb5913d7841486201446a9080
xmasuhai <xmasuhai@163.com>
2021-09-12 14:28:19 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import NewVue from './components/NewVue.vue';
+</script>
+
+<template>
+  <new-vue></new-vue>
+</template>
---
+++ src/components/NewVue.vue
@@ -0,0 +1,13 @@
+<template>
+  第一个组件吧
+</template>
+
+<script lang="ts">
+export default {
+  name: 'NewVue'
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
