bfcfc5951f113b8e6e1867a7a7461b8323c60f94
xmasuhai <xmasuhai@163.com>
2021-09-15 06:03:57 +0800

    :lipstick: 添加 top-nav 样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,13 @@
 <template>
   <div class="top-nav">
-    <div class="logo"></div>
-    <div class="menu"></div>
+    <div class="logo">LOGO</div>
+    <ul class="menu">
+      <li>菜单1</li>
+      <li>菜单2</li>
+    </ul>
   </div>
   <div class="banner">
-    <h1>Vueel 3</h1>
+    <h1>Vueel 3 demo</h1>
     <h2>一个基于 Vue 3 的UI组件库</h2>
     <p class="actions">
       <a href="https://www.github.com">GitHub</a> |
@@ -20,5 +23,24 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.top-nav {
+  background: mediumseagreen;
+  display: flex;
+  padding: 16px;
 
+  > .logo {
+    max-width: 6em;
+    margin-right: auto;
+  }
+
+  > .menu {
+    display: flex;
+    white-space: nowrap;
+    flex-wrap: nowrap;
+
+    > li {
+      margin: 0 1em;
+    }
+  }
+}
 </style>
