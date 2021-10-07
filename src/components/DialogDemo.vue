60bf3cd67d9f499a35dbab972d8cdac50e75d06e
xmasuhai <xmasuhai@163.com>
2021-10-08 06:04:31 +0800

    :sparkles: 创建 Dialog组件

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
+import Dialog from '@/lib/Dialog.vue';
 </script>
 
 <template>
-  <div>
-    DialogDemo
-  </div>
+  <h1>Dialog 示例</h1>
+  <h2>示例一</h2>
+  <Dialog></Dialog>
 </template>
 
 <script lang="ts">
---
+++ src/lib/Dialog.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import Button from '@/lib/Button.vue';
+</script>
+
+<template>
+  <div class="vue-dialog-overlay"></div>
+  <div class="vue-dialog-wrapper">
+    <header>标题</header>
+    <main>
+      <p>第一行字</p>
+      <p>第二行字</p>
+    </main>
+    <footer>
+      <Button>OK</Button>
+      <Button>Cancel</Button>
+    </footer>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'Dialog'
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
