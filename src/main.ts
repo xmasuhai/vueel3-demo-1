e6c290eb7d587fde3c3ca3fabe687cb9bf8d199e
xmasuhai <xmasuhai@163.com>
2021-10-20 06:58:16 +0800

    :lipstick: 使用github-markdown-css为添加文档代码内容样式

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
-import '@/lib/var.scss';
-import '@/styles/index.scss';
-import {createApp} from 'vue';
 import App from '@/App.vue';
-import {router} from '@/router';
 import '@/lib/svg.js';
+import '@/lib/var.scss';
+import {router} from '@/router';
+import '@/styles/index.scss';
+import 'github-markdown-css/github-markdown-light.css';
+import {createApp} from 'vue';
 
 const app = createApp(App);
 app.use(router);
