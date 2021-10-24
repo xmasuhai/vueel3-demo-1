fa2a87b517333c343ca82811479b2735ce42227f
xmasuhai <xmasuhai@163.com>
2021-10-25 06:18:29 +0800

    :sparkles: 简化markdown

--- src/router.ts
+++ src/router.ts
@@ -3,11 +3,12 @@ import DialogDemo from '@/components/DialogDemo.vue';
 import DocsDemo from '@/components/DocsDemo.vue';
 import SwitchDemo from '@/components/SwitchDemo.vue';
 import TabsDemo from '@/components/TabsDemo.vue';
+import GetStarted from '@/markdown/getStarted.md';
+import Install from '@/markdown/install.md';
+import Intro from '@/markdown/intro.md';
 import Docs from '@/views/Docs.vue';
-import GetStarted from '@/views/GetStarted.vue';
 import Home from '@/views/Home.vue';
-import Install from '@/views/Install.vue';
-import Intro from '@/views/Intro.vue';
+
 import {createRouter, createWebHashHistory} from 'vue-router';
 
 const history = createWebHashHistory();
--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,12 @@ const {resolve} = require('path');
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  plugins: [vue({
-    include: [/\.vue$/, /\.md$/],
-  }), Markdown()],
+  plugins: [
+    vue({
+      include: [/\.vue$/, /\.md$/],
+    }),
+    Markdown()
+  ],
   resolve: {
     alias: [
       {
