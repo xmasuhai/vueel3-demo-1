79de7aeee09ec22aeb1e44e6f3f3c8a50420ed42
xmasuhai <xmasuhai@163.com>
2021-11-02 07:36:11 +0800

    :rocket: 半自动部署

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
-cd dist
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin git@github.com:xmasuhai/vueel3-demo-website.git
-git push -f -u origin main
-cd ..
+rm -rf dist &&
+vite build &&
+cd dist &&
+git init &&
+git add . &&
+git commit -m "update" &&
+git branch -M main &&
+git remote add origin git@github.com:xmasuhai/vueel3-demo-website.git &&
+git push -f -u origin main &&
+cd -
