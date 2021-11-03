139b07229288f567cbd4dc04f8f272d65ded3385
xmasuhai <xmasuhai@163.com>
2021-11-03 15:14:26 +0800

    :rocket: 使用GitHub Actions CI/CD 自动化部署到 gh-pages 分支

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,8 @@ cd dist &&
 git init &&
 git add . &&
 git commit -m "update" &&
-git branch -M main &&
+git branch -M gh-pages &&
 git remote add origin git@github.com:xmasuhai/vueel3-demo-website.git &&
-git push -f -u origin main &&
+git push -f -u origin gh-pages &&
 cd -
 echo https://xmasuhai.xyz/vueel3-demo-website/index.html
