4b48aea51cf22b09a883da748e04d150c1da54f8
xmasuhai <xmasuhai@163.com>
2021-11-05 05:53:16 +0800

    :bulb: :package: :wrench: :chart_with_upwards_trend: 更新rollup打包配置

--- rollup.config.js
+++ rollup.config.js
@@ -4,25 +4,31 @@
 // rollup-plugin-scss
 // sass
 // rollup-plugin-terser
+import path from 'path'
 import dartSass from 'sass'
 import vuePlugin from 'rollup-plugin-vue'
 import esbuildPlugin from 'rollup-plugin-esbuild'
 import scssPlugin from 'rollup-plugin-scss'
+import bundleSize from 'rollup-plugin-filesize'
+import alias from '@rollup/plugin-alias'
 import {terser} from 'rollup-plugin-terser' // 压缩 js 代码，包括 ES6 代码压缩
+import pkg from './package.json'
+
+const pathResolve = p => path.resolve(__dirname, p)
 
 export default {
   input: 'src/lib/index.ts',
   output: {
     // exports: 'vueel3',
     globals: {
-      vue: 'vue' // 告诉rollup全局变量Vue即是vue
+      vue: 'Vue' // 告诉rollup全局变量Vue即是vue
     },
     name: 'vueel3-ui',
     file: 'dist/lib/vueel3.js',
     format: 'umd', // format: 'cjs',
     plugins: [terser()],
   },
-  external: ['vue'],
+  external: [Object.keys(pkg.dependencies)],
   plugins: [
     // .vue -> .js
     vuePlugin({
@@ -30,7 +36,11 @@ export default {
       // 把单文件组件中的样式，插入到html中的style标签
       css: true,
       // 把组件转换成 render 函数
-      compileTemplate: true
+      compileTemplate: true,
+      template: {
+        isProduction: !process.env.ROLLUP_WATCH,
+        compilerOptions: {preserveWhitespace: false}
+      }
     }),
     // .ts -> .js
     esbuildPlugin({
@@ -39,6 +49,10 @@ export default {
       target: 'es2015'
     }),
     // .scss -> css
-    scssPlugin({include: /\.scss$/, sass: dartSass})
+    scssPlugin({include: /\.scss$/, sass: dartSass}),
+    alias({
+      '@': pathResolve('src')
+    }),
+    bundleSize(),
   ]
 }
