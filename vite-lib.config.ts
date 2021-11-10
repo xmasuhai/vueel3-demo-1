28e67920035aacbb08a1f1aedfc893858b62be04
xmasuhai <xmasuhai@163.com>
2021-11-10 14:55:31 +0800

    :package: 修改 构建库时vite-lib.config.ts

--- vite-lib.config.ts
+++ vite-lib.config.ts
@@ -10,7 +10,7 @@ const libConfig = Object.assign(basicConfig, {
     lib: {
       entry: resolve(__dirname, 'src/lib/index.ts'),
       name: 'vueel3-ui',
-      formats: [/*'es', */'umd'],
+      // formats: [/*'es', */'umd'], // build.rollupOptions.output
       fileName: (/*format*/) => `vueel3-ui.js`  // `vueel3-ui.${format}.js`
     },
     minify: 'terser', // 'terser' 相对较慢，但大多数情况下构建后的文件体积更小。// minify: 'esbuild','esbuild' 最小化混淆更快但构建后的文件相对更大。
