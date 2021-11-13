b5d6de4c025414a597ccf4ae8d77fefe22c77618
xmasuhai <xmasuhai@163.com>
2021-11-13 16:30:15 +0800

    :package: :bug: 修复入口文件index.ts 组件命名错误 发布版本0.0.12

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,10 +1,11 @@
 import './index.scss';
 
-export {default as Switch} from './VlSwitch.vue';
-export {default as Button} from './VlButton.vue';
-export {default as Tabs} from './VlTabs.vue';
-export {default as TabItem} from './VlTabItem.vue';
-export {default as Dialog} from './VlDialog.vue';
+export {default as VlIcon} from './VlIcon.vue';
+export {default as VlButton} from './VlButton.vue';
+export {default as VlSwitch} from './VlSwitch.vue';
+export {default as VlTabs} from './VlTabs.vue';
+export {default as VlTabItem} from './VlTabItem.vue';
+export {default as VlDialog} from './VlDialog.vue';
 export {useDialog as useDialog} from './hooks/useDialog';
 export {useCheckSlots as useCheckSlots} from './hooks/useCheckSlots';
 export {useGetSlots as useGetSlots} from './hooks/useGetSlots';
