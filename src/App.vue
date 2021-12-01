713d598a9e0d5e413e45ee4bd7a8c9329abc9191
xmasuhai <xmasuhai@163.com>
2021-12-01 11:34:58 +0800

    :fire: :bug: 修复图标bug 移除fitScreenSize.ts

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
-import useEmitter from '@/lib/hooks/useEmitter';
-import fitScreenSize from '@/utils/fitScreenSize';
 import {provide, ref} from 'vue';
+import useEmitter from '@/lib/hooks/useEmitter';
 import {router} from '@/router';
 
 const width = document.documentElement.clientWidth;
@@ -11,8 +10,18 @@ provide('asideVisible', asideVisible);
 const emitter = useEmitter();
 provide('evBus', emitter);
 
+const updateSideMenu = () => {
+  // 发布更新侧边栏事件
+  emitter.emit('update:asideVisible');
+  asideVisible.value = false;
+}
+
 router.afterEach(() => {
-  fitScreenSize()
+  // 判断屏幕尺寸是否为移动端，决定侧边栏是否可见 // 屏幕宽度大于768px 显示代码行数 prismjs line-numbers
+  width <= 768
+    ? updateSideMenu()
+    : document.querySelector('body')?.classList.add('line-numbers');
+
 });
 
 </script>
