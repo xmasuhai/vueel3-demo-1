b14f8e57d8511c9ba900ed36f865f2158d3f461d
xmasuhai <xmasuhai@163.com>
2021-12-02 06:47:09 +0800

    :sparkles: 重构 App.vue

--- src/App.vue
+++ src/App.vue
@@ -14,14 +14,13 @@ const updateSideMenu = () => {
   // 发布更新侧边栏事件
   emitter.emit('update:asideVisible');
   asideVisible.value = false;
-}
+};
 
 router.afterEach(() => {
   // 判断屏幕尺寸是否为移动端，决定侧边栏是否可见 // 屏幕宽度大于768px 显示代码行数 prismjs line-numbers
   width <= 768
-    ? updateSideMenu()
-    : document.querySelector('body')?.classList.add('line-numbers');
-
+      ? updateSideMenu()
+      : document.querySelector('body')?.classList.add('line-numbers');
 });
 
 </script>
